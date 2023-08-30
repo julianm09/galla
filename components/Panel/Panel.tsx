c1b8b95845735f0bdd83794d68e98186a15d0f85
Julian Mayes <juliantmayes@gmail.com>
2023-08-30 00:59:24 -0700

    start and loading screen

--- components/Panel/Panel.tsx
+++ components/Panel/Panel.tsx
@@ -3,6 +3,10 @@ import styles from "./panel.module.css";
 
 export default function Panel() {
   return <div className={styles.panel}>
-    <Input label="project" value="" onChange={() => {}}/>
+    <Input label="project" value="" onChange={() => {}}
+    checkbox={false}
+    handleCheckbox={() => {}}
+    checked={false}
+    />
   </div>;
 }
